4129dfeb66f667dd48c54edb42520471a9219ca5
Bharath Marambeed <94624923+Bharath5551@users.noreply.github.com>
2026-01-16 22:08:03 +0530

    Update Session.js

--- models/Session.js
+++ models/Session.js
@@ -1,21 +1,19 @@
 const mongoose = require("mongoose");
 
-const sessionSchema = new mongoose.Schema(
-  {
-    teacherId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true
-    },
-    subject: {
-      type: String,
-      required: true   // 🔴 REQUIRED
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now
-    }
+const sessionSchema = new mongoose.Schema({
+  teacherId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  subject: {
+    type: String,
+    required: true
+  },
+  expiresAt: {
+    type: Date,
+    required: true
   }
-);
+}, { timestamps: true });
 
 module.exports = mongoose.model("Session", sessionSchema);
