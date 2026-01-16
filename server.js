7580bdb033538bf8415d456e16a0cde7377c94a0
Bharath Marambeed <94624923+Bharath5551@users.noreply.github.com>
2026-01-16 08:02:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on 3000");
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
