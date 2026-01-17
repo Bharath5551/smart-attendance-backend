8b49c71a6967fa5fb4f212e0c257cb3834e1c803
Bharath Marambeed <94624923+Bharath5551@users.noreply.github.com>
2026-01-17 22:38:03 +0530

    Update Subject.js

--- models/Subject.js
+++ models/Subject.js
@@ -3,7 +3,11 @@
 const subjectSchema = new mongoose.Schema({
   name: String,
   code: String,
-  facultyId: mongoose.Schema.Types.ObjectId
+  facultyId: {
+    type: mongoose.Schema.Types.ObjectId,   // 🔴 IMPORTANT
+    ref: "User",
+    required: true
+  }
 });
 
 module.exports = mongoose.model("Subject", subjectSchema);
