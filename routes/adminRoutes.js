2f053deaf8923723bd6db3c18bc173da2bec4b7f
Bharath Marambeed <94624923+Bharath5551@users.noreply.github.com>
2026-01-17 20:37:57 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,11 +4,16 @@ const auth = require("../middleware/authMiddleware");
 const {
   createTeacher,
   getAllTeachers,
-  deleteTeacher
+  deleteTeacher,
+  createSubjectForTeacher
 } = require("../controllers/adminController");
 
 router.post("/teacher", auth("admin"), createTeacher);
 router.get("/teachers", auth("admin"), getAllTeachers);
 router.delete("/teacher/:id", auth("admin"), deleteTeacher);
-
+router.post(
+  "/subject",
+  auth("admin"),
+  createSubjectForTeacher
+);
 module.exports = router;
