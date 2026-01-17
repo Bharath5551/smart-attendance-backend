da4035e424bb7f480c8f6c7f61796f925110e692
Bharath Marambeed <94624923+Bharath5551@users.noreply.github.com>
2026-01-17 19:18:33 +0530

    Create adminController.js

---
+++ routes/adminController.js
@@ -0,0 +1,27 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+exports.createTeacher = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  const teacher = await User.create({
+    name,
+    email,
+    password: hashed,
+    role: "teacher"
+  });
+
+  res.json({ message: "Teacher created", teacher });
+};
+
+exports.getAllTeachers = async (req, res) => {
+  const teachers = await User.find({ role: "teacher" }).select("-password");
+  res.json(teachers);
+};
+
+exports.deleteTeacher = async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.json({ message: "Teacher deleted" });
+};
