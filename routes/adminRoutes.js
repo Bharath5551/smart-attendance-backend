78ba98806caa59bd6bf64d350b1c11ac72f8109f
Bharath Marambeed <94624923+Bharath5551@users.noreply.github.com>
2026-01-18 16:33:33 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -10,7 +10,7 @@ const {
 
 // admin assigns subject
 router.post("/subject", auth("admin"), createSubjectForTeacher);
-
+router.get("/teachers", auth("admin"), getAllTeachers);
 // admin views teacher subjects
 router.get("/teacher/:id/subjects", auth("admin"), getSubjectsByTeacher);
 
