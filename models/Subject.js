b320eafd8c0a24d0223140534c64b47041e722cf
Bharath Marambeed <94624923+Bharath5551@users.noreply.github.com>
2026-01-18 16:23:44 +0530

    Update Subject.js

--- models/Subject.js
+++ models/Subject.js
@@ -1,13 +1,19 @@
- const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-const subjectSchema = new mongoose.Schema({
-  name: String,
-  code: String,
+const SubjectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  code: {
+    type: String,
+    required: true
+  },
   facultyId: {
-    type: mongoose.Schema.Types.ObjectId,   // 🔴 IMPORTANT
+    type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true
   }
 });
 
-module.exports = mongoose.model("Subject", subjectSchema);
+module.exports = mongoose.model("Subject", SubjectSchema);
